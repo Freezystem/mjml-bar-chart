64533340fd8902b9bf12b62cafe12c75cea15cc6
Nicolas Polizzo <freezystem@gmail.com>
2023-12-05 01:18:07 +0100

    add test for label

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -12,6 +12,14 @@ describe("mjml-bar-chart", () => {
 		registerComponent(MjBarChart);
 	});
 
+	const attributes = {
+		"dataset-labels": "January,February,March",
+		datasets: "[[33,14,27],[18,66,42],[7,15,21]]",
+		groups: "support,sales,tech",
+		colors: "#ffe5ec,#ffb3c6,#fb6f92",
+	};
+	const barChart = new MjBarChart({ attributes });
+
 	describe("mjml markup", () => {
 		it("should render the bar chart", () => {
 			const mjml = `
@@ -37,18 +45,19 @@ describe("mjml-bar-chart", () => {
 
 	describe("getChartBarSeparator", () => {
 		it("should render chart bar separator", () => {
-			const barChart = new MjBarChart({
-				attributes: {
-					"dataset-labels": "January,February,March",
-					datasets: "[[33,14,27],[18,66,42],[7,15,21]]",
-					groups: "support,sales,tech",
-					colors: "#ffe5ec,#ffb3c6,#fb6f92",
-				},
-			});
-
 			const gen = barChart["getChartBarSeparator"]();
 
 			expect(gen).toBe('<td style="padding:0;min-width:30px;max-width:30px;"></td>');
+		});
+	});
+
+	describe("getLabel", () => {
+		it("should render label", () => {
+			const gen = barChart["getLabel"]("test");
+
+			expect(gen).toBe(
+				'<td style="height:30px;padding:0;font-size:14px;text-align:center;overflow:hidden;min-width:90px;max-width:90px;">test</td>'
+			);
 		});
 	});
 });
