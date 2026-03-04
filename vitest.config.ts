0322cf2e8baf794f657d897b60ade6026b6c40d2
Nico <1469878+Freezystem@users.noreply.github.com>
2026-03-04 13:48:32 +0100

    [FEAT] replace jest by vitest (#113)
    * replace jest by vitest
    
    * upgrade biomejs to v2.4.5
    
    * require npm 11

---
+++ vitest.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+    test: {
+        reporters: process.env.GITHUB_ACTIONS
+            ? ["dot", "github-actions"]
+            : ["tree"],
+        watch: false,
+        fileParallelism: true,
+        maxWorkers: "50%",
+        include: ["src/**/*.spec.ts"],
+        coverage: {
+            provider: "v8",
+            reporter: ["text", "html", "cobertura"],
+            reportsDirectory: "./coverage",
+            reportOnFailure: true,
+            include: ["src/**/*.ts"],
+            exclude: ["src/**/*.test.ts", "src/**/*.d.ts"],
+            thresholds: {
+                functions: 100,
+                lines: 100,
+                statements: 100,
+                branches: 100,
+            },
+        },
+    },
+});
